bc5658b33e3732ec64db38bfdee403c504637805
diyarulin <s.qwanta@gmail.com>
2024-12-20 17:13:44 +0500

    4th lesson from youtube

--- main.go
+++ main.go
@@ -2,27 +2,55 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type Posts struct {
-	id       uint16
-	title    string
-	context  string
-	username string
-	date     string
+	ID   int
+	Name string
+	Body string
+	Date string
+	User []string
 }
 
-func (p *Posts) getAllInfo() string {
-	return fmt.Sprintf("id is: %d, title is: %s, context is: %s, username is: %s, date is: %s", p.id, p.title, p.context, p.username, p.date)
+var posts = Posts{
+	ID:   1,
+	Name: "Welcome to My Blog",
+	Body: "This is the first post on my new blog. I will share my thoughts on various topics.",
+	Date: "2024-10-19",
+	User: []string{"John Doe", "Didar"},
 }
-func (p *Posts) editTitle(newTitle string) {
-	p.title = newTitle
+
+// {
+// 	ID:   2,
+// 	Name: "Understanding Go Structs",
+// 	Body: "In this post, we will dive into how structs work in Go and how to use them efficiently.",
+// 	Date: "2024-10-20",
+// 	User: "Jane Smith",
+// },
+// {
+// 	ID:   3,
+// 	Name: "Working with HTTP in Go",
+// 	Body: "Let's explore how to build a simple web server in Go using the net/http package.",
+// 	Date: "2024-10-21",
+// 	User: "DevGuru",
+// },
+// }
+
+func (p *Posts) getAllInfo() string {
+	return fmt.Sprintf("id is: %d, title is: %s, context is: %s,  date is: %s, username is: %s", p.ID, p.Name, p.Body, p.Date, p.User)
+}
+func (p *Posts) editName(newTitle string) {
+	p.Name = newTitle
 }
 func home_page(w http.ResponseWriter, r *http.Request) {
-	Post := Posts{1, "How to write forum", "1st step, 2nd step", "David", "2017-08-31"}
-	Post.editTitle("123")
-	fmt.Fprint(w, Post.getAllInfo())
+	// Post := Posts{1, "How to write forum", "1st step, 2nd step", "David", "2017-08-31"}
+	// // Post.editTitle("123")
+	// fmt.Fprint(w, `<h1>Main Text</h1>
+	// <b>Main</b>`)
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, posts)
 }
 func about_page(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "About us")
